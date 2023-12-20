f0c1fd68a5ed4127ce09c030b5a104cac623dd7e
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2023-12-20 20:32:05 +0100

    Fix db

--- app/access/users.py
+++ app/access/users.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Users"""
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 from pydantic import BaseModel
 
 router = APIRouter()
@@ -13,14 +13,29 @@ class User(BaseModel):
     admin: bool = False
 
 
-fake_db = {
-    "rberga06":    User(username="rberga06", hashed_password="fakehashed@password", admin=True),
-    "madda":       User(username="madda", hashed_password="fakehashed@password", admin=True),
-    "TommyErBono": User(username="TommyErBono", hashed_password="fakehashed@password"),
-}
+fake_db: dict[str, User] = {}
+
+
+async def get_user(username: str, /) -> User | None:
+    if username in fake_db:
+        return fake_db[username]
 
 
 @router.get("/users/@{username}")
-async def get_user(username: str) -> User | None:
-    if username in fake_db:
-        return fake_db[username]
+async def get_user_unsafe(username: str) -> User:
+    user = await get_user(username)
+    if user is None:
+        raise HTTPException(status.HTTP_404_NOT_FOUND)
+    return user
+
+
+@router.get("/users/init")
+async def init_db():
+    from .auth import hash_password
+    for user in [
+        User(username="rberga06", hashed_password=hash_password("password"), admin=True),
+        User(username="madda", hashed_password=hash_password("password"), admin=True),
+        User(username="TommyErBono", hashed_password=hash_password("password")),
+    ]:
+        fake_db[user.username] = user
+    return {"result": "Everything ok!"}
