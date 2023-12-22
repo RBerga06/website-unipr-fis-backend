8fc4fdd2067f7abec9416eac7f39ba7cfeffd078
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2023-12-22 23:51:29 +0100

    Add CORSMiddleware

--- app/main.py
+++ app/main.py
@@ -3,7 +3,14 @@
 """The backend website. Powered by FastAPI + Pydantic + SQLModel."""
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import access, routers
+
+
+ORIGINS = [
+    "http://localhost",
+    "http://localhost:5173",
+]
 
 
 @asynccontextmanager
@@ -20,6 +27,13 @@ async def lifespan(app: FastAPI, /):
 app = FastAPI(lifespan=lifespan)
 app.include_router(access.auth.router)
 app.include_router(routers.users.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
